d6596738714bb71fed2ad98282f8a69c99f2152e
Семененко Надежда <tn@impsa.ru>
2022-08-25 18:26:53 +0300

    feat: add layout & styles

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import './index.html';
-import './index.scss';
-import code from './img/code.png'
-import { mult, sum } from './modules/calc';
+import './index.css';
+// import './assets'
+// import code from './img/code.png'
+// import { mult, sum } from './modules/calc';
 
-const imgWrap = document.querySelector('.img');
-const img = new Image();
-img.src = code;
-img.width = 700;
-imgWrap.append(img);
+// const imgWrap = document.querySelector('.img');
+// const img = new Image();
+// img.src = code;
+// img.width = 700;
+// imgWrap.append(img);
 
-console.log(mult(3, 4));
-console.log(sum(3, 4));
+// console.log(mult(3, 4));
+// console.log(sum(3, 4));
+
+import { ancientsData } from "./data/ancients.js"
+import { difficulties } from "./data/difficulties.js"
+import { brownCards, blueCards, greenCards } from "./data/mythicCards/index.js"
+
+console.log(Ancients)
+console.log(Diff)
+console.log(brownCards)
+console.log(blueCards)
+console.log(greenCards)
