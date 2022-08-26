400680f3b85b06a0c63243960a8e9d602d02f95a
Семененко Надежда <tn@impsa.ru>
2022-08-26 14:02:02 +0300

    feat: start add select for cards & levels

--- src/index.js
+++ src/index.js
@@ -22,3 +22,61 @@ console.log(difficulties)
 console.log(brownCards)
 console.log(blueCards)
 console.log(greenCards)
+
+
+console.log(typeof(brownCards));
+
+//1. функция смешивания массива
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
+shuffle(brownCards);
+
+
+
+// let brownCardsArr =  brownCards.keys(brownCards);
+
+// console.log(brownCardsArr);
+// console.log(brownCards.length);
+
+
+//Если кликнули на карту Азатора
+// ancientsData[0]
+
+const cardsContainer = document.querySelector('.cards__container');
+const levelBtnContainer  = document.querySelector('.level-btn__container');
+// const card = document.querySelector('.card');
+// const cardAzathor = document.querySelector('.card_azathoth');
+
+let selectedCard;
+
+
+function activeCard(item) {
+  if (selectedCard) { // убрать существующую подсветку, если есть
+    selectedCard.classList.remove('card_active');
+  } selectedCard = item;
+   selectedCard.classList.add('card_active'); // подсветить новый td
+}
+
+
+cardsContainer.onclick = function(event) {
+  let card = event.target.closest('.card');
+  if (!card) return;
+  activeCard(card);
+};
+
+let selectedLevel;
+
+function activeLevel(item) {
+  if (selectedLevel) { 
+    selectedLevel.classList.remove('level_active');
+  } selectedLevel = item;
+  selectedLevel.classList.add('level_active');
+}
+
+levelBtnContainer.onclick = function(event) {
+  let levelBtn = event.target.closest('.level-btn');
+  if (!levelBtn) return;
+  activeLevel(levelBtn);
+};
